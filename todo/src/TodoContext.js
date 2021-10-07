1da772f98a1cbf501bee2a2a7d20a60f76757654
KGH <kgh2703@irlink.co.kr>
2021-10-07 14:03:47 +0900

    수정

--- todo/src/TodoContext.js
+++ todo/src/TodoContext.js
@@ -20,18 +20,26 @@ function todoReducer(state, action) {
 }
 
 const initialTodos = [
-  {
-    id: 1,
-    text: '아침 산책',
-    done: true
-  },
-  {
-    id: 2,
-    text: '오늘의 뉴스 읽기',
-    done: true
-  },
-  { id: 3, text: '샌드위치 사 먹기', done: false },
-  { id: 4, text: '리액트 공부하기', done: false }
+    {
+        id: 1,
+        text: '손소독',
+        done: true
+    },
+    {
+        id: 2,
+        text: 'React 공부',
+        done: true
+    },
+    { 
+        id: 3, 
+        text: '기능 구현', 
+        done: false 
+    },
+    { 
+        id: 4, 
+        text: '수면', 
+        done: false 
+    }
 ];
 
 export function TodoProvider({ children }) {
