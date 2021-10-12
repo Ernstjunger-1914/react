88a352b20c1e5ee60d7f346acc374bcd739cbd95
KGH <kgh2703@irlink.co.kr>
2021-10-12 11:34:36 +0900

    DB 연동

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,18 +1,33 @@
+import { useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  const [postname, setPostName]=useState('');
+  const [main, setMain]=useState('');
+
+  const submitpost=()=> {
+    Axios.post('http://localhost:3033/api/insert', {postname: postname, main: main}).then(()=> {
+      alert("successful");
+    });
+  }
+
   return (
     <div className="App">
       <h1>CRUD</h1>
 
       <div className="form">
         <label>Content Name</label>
-        <input type="text" name="post-name" />
+        <input type="text" name="postname" onChange={(e)=> {
+          setPostName(e.target.value)
+        }} />
 
         <label>Main</label>
-        <input type="text" name="main" />
+        <input type="text" name="main"  onChange={(e)=> {
+          setMain(e.target.value)
+        }} />
 
-        <button>Submit</button>
+        <button onClick={submitpost}>Submit</button>
       </div>
     </div>
   );
--- app/server/index.js
+++ app/server/index.js
@@ -1,8 +1,32 @@
 const express=require('express')
 const app=express();
+const mysql=require('mysql');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+
+const db=mysql.createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "0000",
+    database: "ssd"
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res)=> {
     res.send("손소독");
+});
+
+app.post('/api/insert', (req, res)=> {
+    const postname=req.body.postname;
+    const main=req.body.main;
+
+    const sqlinsert="insert into ssd.content(content_name, main) values(?, ?)"
+    db.query(sqlinsert, [postname, main], (err, result)=> {
+        console.log(result);
+    });
 });
 
 app.listen(3033, ()=>{
