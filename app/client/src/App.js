7ea691c6d8f21e9f79e14d3e54563f095751d517
KGH <vot768@naver.com>
2021-10-22 10:31:14 +0900

    route는 된다

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,85 +1,25 @@
-import { useEffect, useState } from 'react';
-import { Route, Link, Router, Switch } from 'react-router-dom';
-import Axios from 'axios';
+import { React } from 'react';
+import { Route, Link, Router, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Login from './Login';
 import Register from './Register';
+import Todo from './Todo';
+
 
 function App() {
 
-  const [postname, setPostName]=useState('');
-  const [main, setMain]=useState('');
-  const [postList, setPostList]=useState([]);
-  const [newMain, setNewMain]=useState('');
-  Axios.defaults.withCredentials = true;
-
-  useEffect(()=> {
-    Axios.get('http://localhost:3033/api/get').then((Response)=> {
-      setPostList(Response.data);
-    });
-  }, []);
-
-  const submitpost=()=> {
-    if(postname==='') {
-      alert('제목을 입력해주시오.');
-
-      return false;
-    } else if(main==='') {
-      alert("내용을 입력해주시오.");
-
-      return false;
-    }
-    
-    Axios.post('http://localhost:3033/api/insert', {postname: postname, main: main}).then(()=> {
-        setPostList([...postList, {postname: postname, main: main},
-      ]);
-    });
-  }
-
-  const deletePost=(post)=> {
-    Axios.delete(`http://localhost:3033/api/delete/${post}`);
-  }
-
-  const updatePost=(post)=> {
-    Axios.patch('http://localhost:3033/api/update', {
-      postname: post,
-      main: newMain,
-    }).then(() => {
-      setNewMain('')
-    });
-  }
-
   return (
     <div className="App">
-      <nav className="header"></nav>
+      <nav className="header">
+      </nav>
 
-      <div className="form">
-        <label>Content Name</label>
-        <input type="text" name="postname" onChange={(e)=> {
-          setPostName(e.target.value)
-        }} />
-
-        <label>Main</label>
-        <input type="text" name="main" onChange={(e)=> {
-          setMain(e.target.value)
-        }} />
-
-        <button onClick={submitpost}>Submit</button>
-
-        {postList.map((val)=> {
-          return (
-            <div className="card">
-              <h1>{val.postname}</h1>
-              <p>{val.main}</p>
-              <button onClick={()=> {deletePost(val.postname)}}>Delete</button>
-              <input type="text" id="updateinput" onChange={(e)=> {
-                setNewMain(e.target.value)
-              }} />
-              <button onClick={()=> {updatePost(val.postname)}}>Update</button>
-            </div>
-          );
-        })}
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/todo' component={Todo} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} /> 
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ app/client/src/Todo.js
@@ -0,0 +1,83 @@
+import { React, useEffect, useState } from 'react';
+import Axios from 'axios';
+import './App.css';
+
+function Todo() {
+
+  const [postname, setPostName]=useState('');
+  const [main, setMain]=useState('');
+  const [postList, setPostList]=useState([]);
+  const [newMain, setNewMain]=useState('');
+  Axios.defaults.withCredentials = true;
+
+  useEffect(()=> {
+    Axios.get('http://localhost:3033/api/get').then((Response)=> {
+      setPostList(Response.data);
+    });
+  }, []);
+
+  const submitpost=()=> {
+    if(postname==='') {
+      alert('제목을 입력해주시오.');
+
+      return false;
+    } else if(main==='') {
+      alert("내용을 입력해주시오.");
+
+      return false;
+    }
+    
+    Axios.post('http://localhost:3033/api/insert', {postname: postname, main: main}).then(()=> {
+        setPostList([...postList, {postname: postname, main: main},
+      ]);
+    });
+  }
+
+  const deletePost=(post)=> {
+    Axios.delete(`http://localhost:3033/api/delete/${post}`);
+  }
+
+  const updatePost=(post)=> {
+    Axios.patch('http://localhost:3033/api/update', {
+      postname: post,
+      main: newMain,
+    }).then(() => {
+      setNewMain('')
+    });
+  }
+
+  return (
+    <div className="App">
+
+      <div className="form">
+        <label>Content Name</label>
+        <input type="text" name="postname" onChange={(e)=> {
+          setPostName(e.target.value)
+        }} />
+
+        <label>Main</label>
+        <input type="text" name="main" onChange={(e)=> {
+          setMain(e.target.value)
+        }} />
+
+        <button onClick={submitpost}>Submit</button>
+
+        {postList.map((val)=> {
+          return (
+            <div className="card">
+              <h1>{val.postname}</h1>
+              <p>{val.main}</p>
+              <button onClick={()=> {deletePost(val.postname)}}>Delete</button>
+              <input type="text" id="updateinput" onChange={(e)=> {
+                setNewMain(e.target.value)
+              }} />
+              <button onClick={()=> {updatePost(val.postname)}}>Update</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
